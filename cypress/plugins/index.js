ec150786fa98a12a853fbbf1f1a8e0ebc4c60cc0
mlisboaPetLove <matheus.lisboa@petlove.com.br>
2022-05-20 14:46:06 -0300

    configuracao do plugin do cucumber + padronizacao do projeto para o fomrato PageObject

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -20,3 +20,11 @@
   module.exports = (on, config) => {
     
 }
+
+const cucumber = require("cypress-cucumber-preprocessor").default;
+const browserify = require("@cypress/browserify-preprocessor");
+
+module.exports = (on) => {
+    const options = browserify.defaultOptions;
+    on("file:preprocessor", cucumber(options));
+};
