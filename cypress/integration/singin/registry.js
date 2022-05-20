ef61aac43382f0c695f96d1eb39af91cefa72329
Wesley Coutinho Correa da Silva <wesley.silva@keeggo.com>
2022-05-20 14:21:41 -0300

    Acrescentado cenario de email invalido

--- cypress/integration/singin/registry.js
+++ cypress/integration/singin/registry.js
@@ -3,44 +3,51 @@
 const Faker = require('faker')
 
 describe("User Registry", () =>{
-let user
+    let user
 
-before(() =>{
-    user = {email: Faker.Internet.email()}
-    cy.visit("?controller=authentication&back=my-account");
-})
-
-    it("Informar um email valido", () =>{
-        cy.get('#email_create').type(`${user.email}{enter}`)
+    before(() =>{
+        user = {email: Faker.Internet.email(),
+                    name: {
+                        first: Faker.Name.firstName(),
+                        last: Faker.Name.lastName()
+                    }}
+        cy.visit("?controller=authentication&back=my-account");
     })
 
-    it("preencher o formulario de cadastro", () =>{
-        cy.url().should("include","#account-creation")
+        it("Informar um email valido", () =>{
+            cy.get('#email_create').type(`${user.email}{enter}`)
+        })
 
-        cy.get('#email').should('include.value', user.email)
+        it("preencher o formulario de cadastro", () =>{
+            cy.url().should("include","#account-creation")
+            cy.get('#email').should('have.value', user.email)
+            cy.get('#id_gender1').check();
+            cy.get('#customer_firstname').type(user.name.first);
+            cy.get('#customer_lastname').type(user.name.last);
+            cy.get('#passwd').type("Abc123$%");
+            cy.get('#address1').type(Faker.Address.streetAddress());
+            cy.get('#city').type(Faker.Address.city());
+            cy.get('#id_state').select("3");
+            cy.get('#postcode').type("12451");
+            cy.get('#phone_mobile').type("11997894512");
 
-        cy.get('#id_gender1').check();
-        cy.get('#customer_firstname').type(Faker.Name.firstName());
-        cy.get('#customer_lastname').type(Faker.Name.lastName());
-        cy.get('#passwd').type("Abc123$%");
 
-        cy.get('#address1').type(Faker.Address.streetAddress());
-        cy.get('#city').type(Faker.Address.city());
-        cy.get('#id_state').select("3");
-        cy.get('#postcode').type("12451");
-        cy.get('#phone_mobile').type("11997894512");
+        })
 
+        it("Finalizar o cadastro do usuario", () =>{
+            cy.get('#submitAccount > span').click();
+            cy.get('.account > span').should('have.text', `${user.name.first} ${user.name.last}`);
+        })
+
+
+        it("Informar um email invalido", () =>{
+            cy.get('#email_create').type(`${user.email}{enter}`)
+        })
+
+        // it("Validar obrigatoriedade do formulario", () =>{
+        //     cy.get('#email_create').type(`${Faker.Internet.email()}{enter}`)
+        //      .then(cy.get('#submitAccount > span').click())
+        //         .then(cy.get('#center_column > div').should("be.visible"))
+        // })
 
     })
-
-    it("Finalizar o cadastro do usuario", () =>{
-        cy.get('#submitAccount > span').click();
-    })
-
-    // it("Validar obrigatoriedade do formulario", () =>{
-    //     cy.get('#email_create').type(`${Faker.Internet.email()}{enter}`)
-    //      .then(cy.get('#submitAccount > span').click())
-    //         .then(cy.get('#center_column > div').should("be.visible"))
-    // })
-
-})
